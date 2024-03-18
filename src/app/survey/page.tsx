bea0dc7000538c262bb833ab3bbccb33e3e70e9a
isaiah majarreis <80748304+isaiahmajarreis@users.noreply.github.com>
2024-03-18 13:10:09 +0800

    Implemented survey questions
    Created a new file that includes the const array of all the survey questions, made them display dynamically alongside the progress bar, and a few fixes.

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -5,9 +5,11 @@ import { useState } from 'react';
 import LikertScale from '../../../components/likert-scale/likert-scale.component';
 import ProgressBar from '../../../components/progress-bar/progress-bar.component';
 import YellowContainer from '../../../components/yellow-container/yellow-container.component';
+import SurveyQuestions from './surveyQuestions.tsx';
 
 export default function Survey() {
-  const [active, setActive] = useState(1);
+  const [active, setActive] = useState(0);
+  const [currentQuestionId, setCurrentQuestionId] = useState(0);
   const form = useForm<SurveyForm>({
     initialValues: {
       currentQuestionId: 0,
@@ -21,8 +23,11 @@ export default function Survey() {
         <Title>Survey</Title>
 
         <ProgressBar active={active} setActive={setActive} />
+        <h1 size="lg" style={{ marginBottom: '1rem' }}>
+              {SurveyQuestions[active]} {/* Displays the current question*/}
+          </h1>
 
-        <LikertScale form={form} currentQuestionId={0} />
+        <LikertScale form={form} currentQuestionId={setActive} />
       </Stack>
     </YellowContainer>
   );
