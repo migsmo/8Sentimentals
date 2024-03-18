6f29d68be3c9bedd49d78bc820e76a335c39f229
migsmo <97015261+migsmo@users.noreply.github.com>
2024-03-18 23:05:26 +0800

    UPDATE survey page

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,33 +1,67 @@
 'use client';
-import { Stack, Title } from '@mantine/core';
+import { Button, Container, Paper, Stack, Title } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LikertScale from '../../../components/likert-scale/likert-scale.component';
 import ProgressBar from '../../../components/progress-bar/progress-bar.component';
 import YellowContainer from '../../../components/yellow-container/yellow-container.component';
-import SurveyQuestions from './surveyQuestions.tsx';
+import { SurveyQuestionsData } from '../../../data/survey-questions';
+import { SurveyForm, SurveyQuestion } from '../../../interfaces';
 
 export default function Survey() {
   const [active, setActive] = useState(0);
-  const [currentQuestionId, setCurrentQuestionId] = useState(0);
+  const [randomizedSurveyQuestions, setRandomizedSurveyQuestions] = useState<
+    SurveyQuestion[]
+  >([]);
   const form = useForm<SurveyForm>({
     initialValues: {
-      currentQuestionId: 0,
+      currentQuestionIdx: 0,
       responses: [],
     },
   });
 
+  useEffect(() => {
+    const randomized = SurveyQuestionsData.sort(() => Math.random() - 0.5);
+    setRandomizedSurveyQuestions(randomized);
+  }, []);
+
+  console.log(form.values);
+
   return (
     <YellowContainer alignItems='flex-start' justifyContent='flex-start'>
-      <Stack w='100%'>
-        <Title>Survey</Title>
+      <Stack w='100%' justify='space-between' h='100%'>
+        <Container fluid w='100%'>
+          <Title>Survey</Title>
+        </Container>
+
+        <Paper
+          style={{
+            background: 'transparent',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          py={'10%'}
+        >
+          <Stack>
+            {randomizedSurveyQuestions.length > 0 && (
+              <Stack w='100%' align='center' px='xl'>
+                <Title order={2} pb='xl'>
+                  {randomizedSurveyQuestions[0].question}
+                </Title>
+                <LikertScale
+                  form={form}
+                  question={
+                    randomizedSurveyQuestions[form.values.currentQuestionIdx]
+                  }
+                />
+              </Stack>
+            )}
+            <Button color='black'>Next question</Button>
+          </Stack>
+        </Paper>
 
         <ProgressBar active={active} setActive={setActive} />
-        <h1 size="lg" style={{ marginBottom: '1rem' }}>
-              {SurveyQuestions[active]} {/* Displays the current question*/}
-          </h1>
-
-        <LikertScale form={form} currentQuestionId={setActive} />
       </Stack>
     </YellowContainer>
   );
