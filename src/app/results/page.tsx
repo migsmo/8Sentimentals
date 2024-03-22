55c1da2aef532ece4aeb1542e4f7abc5fcb04860
migsmo <97015261+migsmo@users.noreply.github.com>
2024-03-22 21:39:03 +0800

    FIX code to be buildable

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,18 +1,23 @@
-'use client'
-import { Stack, Title } from '@mantine/core';
-import { useState, useEffect } from 'react';
-import { EmotionCategory } from '../../../enums/index';
-import { SurveyQuestion, SurveyQuestionsData } from '../../../data/survey-questions';
-import { finalOutcomes } from '../../../data/final-outcomes';
+'use client';
+import { Title } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import CenterContainer from '../../../components/yellow-container/yellow-container.component';
+import { finalOutcomes } from '../../../data/final-outcomes';
+import { SurveyQuestionsData } from '../../../data/survey-questions';
+import { EmotionCategory } from '../../../enums/index';
+import { SurveyResponse } from '../../../interfaces';
 
-export default function Results(){
+export default function Results() {
   const surveyResponsesString = localStorage.getItem('surveyState');
-  const surveyResponses = JSON.parse(surveyResponsesString || '{}').responses;
+  const surveyResponses: SurveyResponse[] = JSON.parse(
+    surveyResponsesString || '{}'
+  ).responses;
   // Retrieve survey responses from localStorage
 
   const formattedResponses = surveyResponses.map(({ qId, answer }) => ({
-        qId: parseInt(qId), answer: parseInt(answer)}));
+    qId: qId,
+    answer: parseInt(answer),
+  }));
   // Format survey responses into array
 
   const [emotionTally, setEmotionTally] = useState({
@@ -36,7 +41,9 @@ export default function Results(){
     });
 
     formattedResponses.forEach((response) => {
-      const question = SurveyQuestionsData.find((question) => question.id === response.qId);
+      const question = SurveyQuestionsData.find(
+        (question) => question.id === response.qId
+      );
       if (question) {
         // Increment tally based on answer
         if (response.answer === 1) {
@@ -70,97 +77,133 @@ export default function Results(){
   let ratioA = 0;
   let ratioB = 0;
   let ratioC = 0;
-  let finalEmotions = ''
+  let finalEmotions = '';
   let finalOutcome = '';
 
-  if (emotionTally[EmotionCategory.HAPPINESS] > emotionTally[EmotionCategory.SADNESS]) {
+  if (
+    emotionTally[EmotionCategory.HAPPINESS] >
+    emotionTally[EmotionCategory.SADNESS]
+  ) {
     emotionA = 'H';
-    ratioA = emotionTally[EmotionCategory.HAPPINESS] / (emotionTally[EmotionCategory.SADNESS] + emotionTally[EmotionCategory.HAPPINESS]) * 100;
-} else {
+    ratioA =
+      (emotionTally[EmotionCategory.HAPPINESS] /
+        (emotionTally[EmotionCategory.SADNESS] +
+          emotionTally[EmotionCategory.HAPPINESS])) *
+      100;
+  } else {
     emotionA = 'S';
-    ratioA = emotionTally[EmotionCategory.HAPPINESS] / (emotionTally[EmotionCategory.HAPPINESS] + emotionTally[EmotionCategory.SADNESS]) * 100;
-}
+    ratioA =
+      (emotionTally[EmotionCategory.HAPPINESS] /
+        (emotionTally[EmotionCategory.HAPPINESS] +
+          emotionTally[EmotionCategory.SADNESS])) *
+      100;
+  }
 
-if (emotionTally[EmotionCategory.EXCITEMENT] > emotionTally[EmotionCategory.BOREDOM]) {
+  if (
+    emotionTally[EmotionCategory.EXCITEMENT] >
+    emotionTally[EmotionCategory.BOREDOM]
+  ) {
     emotionB = 'E';
-    ratioB = emotionTally[EmotionCategory.EXCITEMENT] / (emotionTally[EmotionCategory.BOREDOM] + emotionTally[EmotionCategory.EXCITEMENT]) * 100;
-} else {
+    ratioB =
+      (emotionTally[EmotionCategory.EXCITEMENT] /
+        (emotionTally[EmotionCategory.BOREDOM] +
+          emotionTally[EmotionCategory.EXCITEMENT])) *
+      100;
+  } else {
     emotionB = 'B';
-    ratioB = emotionTally[EmotionCategory.EXCITEMENT] / (emotionTally[EmotionCategory.EXCITEMENT] + emotionTally[EmotionCategory.BOREDOM]) * 100;
-}
+    ratioB =
+      (emotionTally[EmotionCategory.EXCITEMENT] /
+        (emotionTally[EmotionCategory.EXCITEMENT] +
+          emotionTally[EmotionCategory.BOREDOM])) *
+      100;
+  }
 
-if (emotionTally[EmotionCategory.AFFECTIONATE] > emotionTally[EmotionCategory.DISTANT]) {
+  if (
+    emotionTally[EmotionCategory.AFFECTIONATE] >
+    emotionTally[EmotionCategory.DISTANT]
+  ) {
     emotionC = 'A';
-    ratioC = emotionTally[EmotionCategory.AFFECTIONATE] / (emotionTally[EmotionCategory.DISTANT] + emotionTally[EmotionCategory.AFFECTIONATE]) * 100;
-} else {
+    ratioC =
+      (emotionTally[EmotionCategory.AFFECTIONATE] /
+        (emotionTally[EmotionCategory.DISTANT] +
+          emotionTally[EmotionCategory.AFFECTIONATE])) *
+      100;
+  } else {
     emotionC = 'D';
-    ratioC = emotionTally[EmotionCategory.AFFECTIONATE] / (emotionTally[EmotionCategory.AFFECTIONATE] + emotionTally[EmotionCategory.DISTANT]) * 100;
-}
+    ratioC =
+      (emotionTally[EmotionCategory.AFFECTIONATE] /
+        (emotionTally[EmotionCategory.AFFECTIONATE] +
+          emotionTally[EmotionCategory.DISTANT])) *
+      100;
+  }
 
-
-  if (emotionA == 'H'){
-    if (emotionB == 'E'){
-      if (emotionC == 'A'){
+  if (emotionA == 'H') {
+    if (emotionB == 'E') {
+      if (emotionC == 'A') {
         finalOutcome = finalOutcomes[0];
         finalEmotions = 'HEA';
-      }
-      else if (emotionC == 'D'){
+      } else if (emotionC == 'D') {
         finalOutcome = finalOutcomes[1];
         finalEmotions = 'HED';
       }
-    }
-    else if (emotionB == 'B'){
-      if (emotionC == 'A'){
+    } else if (emotionB == 'B') {
+      if (emotionC == 'A') {
         finalOutcome = finalOutcomes[2];
         finalEmotions = 'HBA';
-      }
-      else if (emotionC == 'D'){
+      } else if (emotionC == 'D') {
         finalOutcome = finalOutcomes[3];
         finalEmotions = 'HBD';
       }
     }
   }
 
-  if (emotionA == 'S'){
-    if (emotionB == 'E'){
-      if (emotionC == 'A'){
+  if (emotionA == 'S') {
+    if (emotionB == 'E') {
+      if (emotionC == 'A') {
         finalOutcome = finalOutcomes[4];
         finalEmotions = 'SEA';
-      }
-      else if (emotionC == 'D'){
+      } else if (emotionC == 'D') {
         finalOutcome = finalOutcomes[5];
         finalEmotions = 'SED';
       }
-    }
-    else if (emotionB == 'B'){
-      if (emotionC == 'A'){
+    } else if (emotionB == 'B') {
+      if (emotionC == 'A') {
         finalOutcome = finalOutcomes[6];
         finalEmotions = 'SBA';
-      }
-      else if (emotionC == 'D'){
-        finalOutcome = finalOutcomes[7]; 
+      } else if (emotionC == 'D') {
+        finalOutcome = finalOutcomes[7];
         finalEmotions = 'SBD';
       }
     }
   }
 
-
-
   return (
-  <CenterContainer>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-      <div style={{ display: 'flex' }}>
-        <Title style={{ fontSize: '10rem'}}>{finalEmotions}</Title>
+    <CenterContainer>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <div style={{ display: 'flex' }}>
+          <Title style={{ fontSize: '10rem' }}>{finalEmotions}</Title>
+        </div>
+        <div>
+          <h2>{`happiness ${ratioA.toFixed(0)}% ${
+            100 - ratioA.toFixed(0)
+          }% sadness`}</h2>
+          <h2>{`excitement ${ratioB.toFixed(0)}% ${
+            100 - ratioB.toFixed(0)
+          }% boredom`}</h2>
+          <h2>{`affectionate ${ratioC.toFixed(0)}% ${
+            100 - ratioC.toFixed(0)
+          }% distant`}</h2>
+        </div>
+        <h1>{finalOutcome}</h1>
+        <div style={{ marginTop: '2rem' }}></div>
       </div>
-      <div>
-        <h2>{`happiness ${ratioA.toFixed(0)}% ${100 - ratioA.toFixed(0)}% sadness`}</h2>
-        <h2>{`excitement ${ratioB.toFixed(0)}% ${100 - ratioB.toFixed(0)}% boredom`}</h2>
-        <h2>{`affectionate ${ratioC.toFixed(0)}% ${100 - ratioC.toFixed(0)}% distant`}</h2>
-      </div>
-      <h1>{finalOutcome}</h1>
-      <div style={{ marginTop: '2rem' }}>
-      </div>
-    </div>
-  </CenterContainer>
-);
+    </CenterContainer>
+  );
 }
