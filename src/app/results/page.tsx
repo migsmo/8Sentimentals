6c7c599b7b00c197ff55b844c9360af8ed59d084
migsmo <97015261+migsmo@users.noreply.github.com>
2024-04-04 17:10:18 +0800

    UPDATE and refactor results page to display AI generated outcome

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -7,24 +7,11 @@ import { EmotionCategory } from '../../../enums/index';
 import { SurveyResponse } from '../../../interfaces';
 
 export default function Results() {
-  const [surveyResponses, setSurveyResponses] = useState<SurveyResponse[]>([]);
-  useEffect(() => {
-    const surveyResponsesString = localStorage.getItem('surveyState');
-    const surveyResponsesParsed: SurveyResponse[] = JSON.parse(
-      surveyResponsesString || '{}'
-    ).responses;
-
-    setSurveyResponses(surveyResponsesParsed);
-  }, []);
   const [finalOutcome, setFinalOutcome] = useState('');
-
-  // Retrieve survey responses from localStorage
-
-  const formattedResponses = surveyResponses.map(({ qId, answer }) => ({
-    qId: qId,
-    answer: parseInt(answer),
-  }));
-  // Format survey responses into array
+  const [ratioA, setRatioA] = useState<number>(0);
+  const [ratioB, setRatioB] = useState<number>(0);
+  const [ratioC, setRatioC] = useState<number>(0);
+  const [finalEmotions, setFinalEmotions] = useState<string>('');
 
   const [emotionTally, setEmotionTally] = useState({
     [EmotionCategory.EXCITEMENT]: 0,
@@ -34,17 +21,18 @@ export default function Results() {
     [EmotionCategory.BOREDOM]: 0,
     [EmotionCategory.HAPPINESS]: 0,
   });
+  const [isDoneInitializing, setIsDoneInitializing] = useState(false);
 
   useEffect(() => {
-    // Reset tally
-    setEmotionTally({
-      [EmotionCategory.EXCITEMENT]: 0,
-      [EmotionCategory.SADNESS]: 0,
-      [EmotionCategory.AFFECTIONATE]: 0,
-      [EmotionCategory.DISTANT]: 0,
-      [EmotionCategory.BOREDOM]: 0,
-      [EmotionCategory.HAPPINESS]: 0,
-    });
+    const surveyResponsesString = localStorage.getItem('surveyState');
+    const surveyResponsesParsed: SurveyResponse[] = JSON.parse(
+      surveyResponsesString || '{}'
+    ).responses;
+
+    const formattedResponses = surveyResponsesParsed.map(({ qId, answer }) => ({
+      qId: qId,
+      answer: parseInt(answer),
+    }));
 
     formattedResponses.forEach((response) => {
       const question = SurveyQuestionsData.find(
@@ -75,76 +63,114 @@ export default function Results() {
         }
       }
     });
-  }, [formattedResponses]);
 
-  let emotionA = '';
-  let emotionB = '';
-  let emotionC = '';
-  let ratioA = 0;
-  let ratioB = 0;
-  let ratioC = 0;
-  let finalEmotions = '';
-
-  if (
-    emotionTally[EmotionCategory.HAPPINESS] >
-    emotionTally[EmotionCategory.SADNESS]
-  ) {
-    emotionA = 'H';
-    ratioA =
-      (emotionTally[EmotionCategory.HAPPINESS] /
-        (emotionTally[EmotionCategory.SADNESS] +
-          emotionTally[EmotionCategory.HAPPINESS])) *
-      100;
-  } else {
-    emotionA = 'S';
-    ratioA =
-      (emotionTally[EmotionCategory.HAPPINESS] /
-        (emotionTally[EmotionCategory.HAPPINESS] +
-          emotionTally[EmotionCategory.SADNESS])) *
-      100;
-  }
-
-  if (
-    emotionTally[EmotionCategory.EXCITEMENT] >
-    emotionTally[EmotionCategory.BOREDOM]
-  ) {
-    emotionB = 'E';
-    ratioB =
-      (emotionTally[EmotionCategory.EXCITEMENT] /
-        (emotionTally[EmotionCategory.BOREDOM] +
-          emotionTally[EmotionCategory.EXCITEMENT])) *
-      100;
-  } else {
-    emotionB = 'B';
-    ratioB =
-      (emotionTally[EmotionCategory.EXCITEMENT] /
-        (emotionTally[EmotionCategory.EXCITEMENT] +
-          emotionTally[EmotionCategory.BOREDOM])) *
-      100;
-  }
-
-  if (
-    emotionTally[EmotionCategory.AFFECTIONATE] >
-    emotionTally[EmotionCategory.DISTANT]
-  ) {
-    emotionC = 'A';
-    ratioC =
-      (emotionTally[EmotionCategory.AFFECTIONATE] /
-        (emotionTally[EmotionCategory.DISTANT] +
-          emotionTally[EmotionCategory.AFFECTIONATE])) *
-      100;
-  } else {
-    emotionC = 'D';
-    ratioC =
-      (emotionTally[EmotionCategory.AFFECTIONATE] /
-        (emotionTally[EmotionCategory.AFFECTIONATE] +
-          emotionTally[EmotionCategory.DISTANT])) *
-      100;
-  }
+    setIsDoneInitializing(true);
+  }, []);
 
   useEffect(() => {
-    // This effect runs after you have calculated the emotional tally
-    // Build the prompt based on the user's responses
+    if (!isDoneInitializing) {
+      return;
+    }
+
+    let emotionA = '';
+    let emotionB = '';
+    let emotionC = '';
+
+    if (
+      emotionTally[EmotionCategory.HAPPINESS] >
+      emotionTally[EmotionCategory.SADNESS]
+    ) {
+      emotionA = 'H';
+      setRatioA(
+        (emotionTally[EmotionCategory.HAPPINESS] /
+          (emotionTally[EmotionCategory.SADNESS] +
+            emotionTally[EmotionCategory.HAPPINESS])) *
+          100
+      );
+    } else {
+      emotionA = 'S';
+      setRatioA(
+        (emotionTally[EmotionCategory.HAPPINESS] /
+          (emotionTally[EmotionCategory.HAPPINESS] +
+            emotionTally[EmotionCategory.SADNESS])) *
+          100
+      );
+    }
+
+    if (
+      emotionTally[EmotionCategory.EXCITEMENT] >
+      emotionTally[EmotionCategory.BOREDOM]
+    ) {
+      emotionB = 'E';
+      setRatioB(
+        (emotionTally[EmotionCategory.EXCITEMENT] /
+          (emotionTally[EmotionCategory.BOREDOM] +
+            emotionTally[EmotionCategory.EXCITEMENT])) *
+          100
+      );
+    } else {
+      emotionB = 'B';
+      setRatioB(
+        (emotionTally[EmotionCategory.EXCITEMENT] /
+          (emotionTally[EmotionCategory.EXCITEMENT] +
+            emotionTally[EmotionCategory.BOREDOM])) *
+          100
+      );
+    }
+
+    if (
+      emotionTally[EmotionCategory.AFFECTIONATE] >
+      emotionTally[EmotionCategory.DISTANT]
+    ) {
+      emotionC = 'A';
+      setRatioC(
+        (emotionTally[EmotionCategory.AFFECTIONATE] /
+          (emotionTally[EmotionCategory.DISTANT] +
+            emotionTally[EmotionCategory.AFFECTIONATE])) *
+          100
+      );
+    } else {
+      emotionC = 'D';
+      setRatioC(
+        (emotionTally[EmotionCategory.AFFECTIONATE] /
+          (emotionTally[EmotionCategory.AFFECTIONATE] +
+            emotionTally[EmotionCategory.DISTANT])) *
+          100
+      );
+    }
+
+    if (emotionA == 'H') {
+      if (emotionB == 'E') {
+        if (emotionC == 'A') {
+          setFinalEmotions('HEA');
+        } else if (emotionC == 'D') {
+          setFinalEmotions('HED');
+        }
+      } else if (emotionB == 'B') {
+        if (emotionC == 'A') {
+          setFinalEmotions('HBA');
+        } else if (emotionC == 'D') {
+          setFinalEmotions('HBD');
+        }
+      }
+    }
+
+    if (emotionA == 'S') {
+      if (emotionB == 'E') {
+        if (emotionC == 'A') {
+          setFinalEmotions('SEA');
+        } else if (emotionC == 'D') {
+          setFinalEmotions('SED');
+        }
+      } else if (emotionB == 'B') {
+        if (emotionC == 'A') {
+          setFinalEmotions('SBA');
+        } else if (emotionC == 'D') {
+          setFinalEmotions('SBD');
+        }
+      }
+    }
+
     const aiPrompt = `Given an emotional tally of happiness: ${
       emotionTally[EmotionCategory.HAPPINESS]
     }, sadness: ${emotionTally[EmotionCategory.SADNESS]}, excitement: ${
@@ -153,7 +179,7 @@ export default function Results() {
       emotionTally[EmotionCategory.AFFECTIONATE]
     }, and distant: ${
       emotionTally[EmotionCategory.DISTANT]
-    }, generate a personalized outcome.`;
+    } and  the final emotions is ${finalEmotions}. Make sure it starts with The outcome of your 8Sentimentals: Emotion Personality Test Simulator is final emotion. Generate a personalized outcome.`;
 
     const fetchData = async () => {
       try {
@@ -164,21 +190,19 @@ export default function Results() {
           },
           body: JSON.stringify({ prompt: aiPrompt }),
         });
-        console.log(response.status, response.statusText);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        // Use the AI-generated text as the final outcome
-        setFinalOutcome(data.choices[0].text);
+
+        setFinalOutcome(data);
       } catch (error) {
-        console.log('Failed to fetch OpenAI response:', error);
         console.error("Failed to fetch OpenAI's response:", error);
       }
     };
 
     fetchData();
-  }, [emotionTally]);
+  }, [isDoneInitializing]);
 
   return (
     <CenterContainer>
